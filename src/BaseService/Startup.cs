d8ce0de3bc7a7cd95e61cd11a5f6ad0d68e45b64
pari <qjfeeling8994@sina.com>
2017-03-06 17:50:20 +0800

    update the Abp project reference local project

--- src/BaseService/Startup.cs
+++ src/BaseService/Startup.cs
@@ -41,7 +41,7 @@ namespace BaseService
             services.AddMvc();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
+        // This method gets called by the  runtime. Use this method to configure the HTTP request pipeline
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             app.UserAbp();
